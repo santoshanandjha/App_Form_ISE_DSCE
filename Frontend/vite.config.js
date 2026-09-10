04ceee1c57795a313aad1df344619d100eca2a4e
Santosh Anand <santoshanand@Santoshs-MacBook-Air.local>
2026-09-10 13:58:43 +0530

    Set root base path in vite.config.js for direct domain access

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/App_Form_ISE_DSCE/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
